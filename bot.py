efc5e24560b1c2a89491ba2a2e48a9015553a746
JW999 <husam.malkawi@hotmail.com>
2017-07-01 01:53:56 +0300

    We are live!

--- bot.py
+++ bot.py
@@ -1,1 +1,25 @@
 import discord
+from discord.ext import commands
+import asyncio
+
+# Prefix used to interact with the bot
+bot_prefix = "?"
+bot_token = "MzMwNDczNDM4MjI3NDY0MTky.DDhhBw.NLI31T6GlQ4CWj4GoLWJxsQnBiw"
+bot_description = """A bot that greets new users, for now."""
+
+
+my_bot = commands.Bot(command_prefix = bot_prefix)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@bot.command()
+async def hello():
+    """Says world"""
+    await bot.say("world")
+
+bot.run(bot_token)
