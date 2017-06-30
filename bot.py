6934215eddf055f45602317c69cf3502cfd5033a
JW999 <husam.malkawi@hotmail.com>
2017-07-01 01:59:18 +0300

    We are live!(for real)

--- bot.py
+++ bot.py
@@ -8,13 +8,13 @@ bot_token = "MzMwNDczNDM4MjI3NDY0MTky.DDhhBw.NLI31T6GlQ4CWj4GoLWJxsQnBiw"
 bot_description = """A bot that greets new users, for now."""
 
 
-my_bot = commands.Bot(command_prefix = bot_prefix)
+bot = commands.Bot(command_prefix = bot_prefix)
 
 @bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
 @bot.command()
