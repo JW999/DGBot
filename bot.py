f567aadb09f8c32f018dc005471e1ea18d869a8b
JW999 <husam.malkawi@hotmail.com>
2017-07-01 03:01:53 +0300

    added basic functionality

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import asyncio
+import random
 
 # Prefix used to interact with the bot
 bot_prefix = "?"
@@ -8,7 +9,7 @@ bot_token = "MzMwNDczNDM4MjI3NDY0MTky.DDhhBw.NLI31T6GlQ4CWj4GoLWJxsQnBiw"
 bot_description = """A bot that greets new users, for now."""
 
 
-bot = commands.Bot(command_prefix = bot_prefix)
+bot = commands.Bot(command_prefix = bot_prefix, description = bot_description)
 
 @bot.event
 async def on_ready():
@@ -21,5 +22,20 @@ async def on_ready():
 async def hello():
     """Says world"""
     await bot.say("world")
+
+@bot.command()
+async def add(left : int, right : int):
+    """Adds two numbers together."""
+    await bot.say(left + right)
+
+
+@bot.command()
+async def coinflip():
+    """A fair 50/50 coin flip"""
+    number = random.randint(1,2)
+    if number == 1:
+        await bot.say("Tails")
+    else:
+        await bot.say("Heads")
 
 bot.run(bot_token)
