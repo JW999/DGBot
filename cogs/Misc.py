f6d22436541ee45b33b9d0146696ea3be5188c40
JW999 <husam.malkawi@hotmail.com>
2017-07-06 21:15:58 +0300

    Added handling for a few errors.

--- cogs/Misc.py
+++ cogs/Misc.py
@@ -2,7 +2,6 @@ import aiohttp
 import asyncio
 import discord
 from discord.ext import commands
-import PIL
 from PIL import Image
 from PIL import ImageOps
 import async_timeout
@@ -18,17 +17,14 @@ class misc:
 
     async def download_img(self, ctx, url, name):
         """Downloads an img from passed url."""
-        try:
-            with async_timeout.timeout(10):
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(url) as r:
-                        if r.status == 200:
-                            with open(name, 'wb') as fp:
-                                fp.write(await r.read())
-                        else:
-                            await ctx.send("Failed to download image.")
-        except asyncio.CancelledError:
-            await ctx.send("I couldn't download the image, please try again.")
+        with async_timeout.timeout(10):
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as r:
+                    if r.status == 200:
+                        with open(name, 'wb') as fp:
+                            fp.write(await r.read())
+                    else:
+                        await ctx.send("Failed to download image.")
 
     @img.command()
     async def invert(self, ctx, url :str):
@@ -42,19 +38,34 @@ class misc:
 
         # Download the image
         img_name = "{}.png".format(url[url.rfind("/")+1:url.rfind(".")])
-        await self.download_img(ctx, url, img_name)
+        try:
+            await self.download_img(ctx, url, img_name)
+        except asyncio.TimeoutError:
+            await ctx.send("Image is too big.")
+            os.remove(img_name)
+            return
 
         # Invert the image
-        image = Image.open(img_name)
-        if image.mode == "RGBA":
-            image.load()
-            r, g, b, a = image.split()
-            image = Image.merge("RGB", (r, g, b))
-            image = ImageOps.invert(image)
-            r, g, b = image.split()
-            image = Image.merge("RGBA", (r, g, b, a))
-        else:
-            image = ImageOps.invert(image)
+        try:
+            image = Image.open(img_name)
+            if image.mode == "RGBA":
+                image.load()
+                r, g, b, a = image.split()
+                image = Image.merge("RGB", (r, g, b))
+                image = ImageOps.invert(image)
+                r, g, b = image.split()
+                image = Image.merge("RGBA", (r, g, b, a))
+            else:
+                image = ImageOps.invert(image)
+        except NotImplementedError:
+            await ctx.send("Image format not supported.")
+            os.remove(img_name)
+            return
+        except OSError:
+            await ctx.send("Link not supported.")
+            os.remove(img_name)
+            return
+
 
         image.save(img_name)
         await ctx.channel.send(file=discord.File(img_name))
