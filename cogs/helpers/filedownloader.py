2ecebc68b33474f6f006a80bc6bd41c3d7ca6634
JW999 <husam.malkawi@hotmail.com>
2019-06-05 04:34:16 +0300

    Reddit memes, and filedowloader helper cog rework

--- cogs/helpers/filedownloader.py
+++ cogs/helpers/filedownloader.py
@@ -2,35 +2,53 @@ import aiohttp
 import asyncio
 import async_timeout
 
-async def save_file(url, name):
-    """Downloads a file and saves it on disk."""
-    with async_timeout.timeout(10):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as resp:
-                if resp.status == 200:
-                    with open(name, 'wb') as fp:
-                        fp.write(await resp.read())
-                else:
-                    await ctx.send("Failed to download file.")
+async def save_file(url:str, dataType:str, filename:str):
+    """Saves file returned by download_file to disk.
+        Similar to Download_file dataType determines whether to retreive 'json'(decoded into a dict), 'text', or 'binary' data."""
+    downloadedData = await download_file(url, dataType)
 
-async def download_file(url):
-    """Downloads a file, but doesn't save it on disk."""
-    with async_timeout.timeout(10):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as resp:
-                if resp.status == 200:
-                    binaryData =  await resp.read()
-                    return binaryData
-                else:
-                    await ctx.send("Failed to download file.")
+    try:
+        if dataType == 'binary':
+            with open(f"{filename}", "wb") as f:
+                print(filename)
+                f.write(downloadedData)
+        elif dataType == 'text':
+            with open(f"{filename}.txt", "w") as f:
+                f.write(downloadedData)
+        elif dataType == 'json':
+            decodedJsonDict =  await resp.json()
+            with open(f"{filename}.json", "w") as f:
+                json.dump(data, f, indent=4)
+        else:
+            print("Invalid dataType. Check function's doc string.")
+    except Exception as exp:
+        exc = f'{type(exp).__name__}: {exp}'
+        print(f'Failed to save file. {exc}. Show this to someRandomeName')
 
-async def download_json_file(url):
-    """Downloads a json file, but doesn't save it on disk."""
-    with async_timeout.timeout(10):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as resp:
-                if resp.status == 200:
-                    jsonFile =  await resp.json()
-                    return jsonFile
-                else:
-                    await ctx.send("Failed to download file.")
+
+async def download_file(url:str, dataType:str):
+    """Downloads a file, but doesn't save it on disk.
+        dataType determines whether to retreive 'json'(decoded into a dict), 'text', or 'binary' data."""
+    try:
+        with async_timeout.timeout(10):
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as resp:
+                    if resp.status == 200:
+                        ### Output depends on dataType ###
+                        if dataType == 'binary':
+                            binaryData =  await resp.read()
+                            return binaryData
+                        elif dataType == 'text':
+                            textData =  await resp.text()
+                            return textData
+                        elif dataType == 'json':
+                            decodedJsonDict =  await resp.json()
+                            return binaryData
+                        else:
+                            print("Invalid dataType. Check function's doc string.")
+                    else:
+                        print("Failed to download file.")
+    except Exception as exp:
+        exc = f'{type(exp).__name__}: {exp}'
+        print(f'Failed to download file. {exc}. Show this to someRandomeName')
+        # await ctx.send(f'Failed to save file {exc}. Show this to someRandomeName')
--- cogs/misc.py
+++ cogs/misc.py
@@ -2,11 +2,12 @@ from .helpers import filedownloader
 import asyncio
 import async_timeout
 import discord
-from discord.ext import commands
+from discord.ext import tasks, commands
 import os
 from PIL import Image
 from PIL import ImageOps
 import json
+import praw
 
 class misc(commands.Cog):
     def __init__(self, bot):
@@ -29,7 +30,7 @@ class misc(commands.Cog):
         # Download the image
         img_name = "{}.png".format(url[url.rfind("/")+1:url.rfind(".")])
         try:
-            await filedownloader.save_file(url, img_name)
+            await filedownloader.save_file(url, "binary", img_name)
         except asyncio.TimeoutError:
             await ctx.send("Image is too big.")
             os.remove(img_name)
@@ -77,50 +78,80 @@ class misc(commands.Cog):
             await ctx.send("Are you expecting me to make an image out of thin air?")
 
 
-class memes(commands.Cog):
-    """A class that sends memes from reddit, maybe bobs later."""
+class reddit(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        # Get top secret reddit credentials
+        with open("data.json") as f:
+            reddit_creds = json.load(f)
+        self.reddit = praw.Reddit(client_id=reddit_creds["Reddit"]["client_id"],
+                                  client_secret=reddit_creds["Reddit"]["client_secret"],
+                                  username=reddit_creds["Reddit"]["username"],
+                                  password=reddit_creds["Reddit"]["password"],
+                                  user_agent='Meme script by JW999')
+
+        self.dankHotLinkGenerator =  self.memeLinkHotGenerator('dankmemes')
+        self.dankTopLinkGenerator =  self.memeLinkTopGenerator('dankmemes')
 
     @commands.group(invoke_without_command=True)
     async def memes(self, ctx):
-        await ctx.send("I still haven't added commands to the help page, as Husam about them.")
+        await ctx.send("Please refer to the help command for a list of subcommands.")
 
     @memes.command()
-    async def topDank(self, ctx):
-        """Downloads the top meme of the day from /r/dankmemes"""
-        name = "top.json"
-        url = "https://www.reddit.com/r/dankmemes/top.json"
-
+    async def dank(self,ctx):
         try:
-            redditData = await filedownloader.download_json_file(url)
-        except asyncio.TimeoutError:
-            await ctx.send("A connection error has occured.")
-            return
+            link =  next(self.dankHotLinkGenerator)
+            await self.send_picture(ctx, link)
+        except StopIteration:
+            self.dankHotLinkGenerator = await self.memeLinkHotGenerator('dankmemes')
+            await self.dank(ctx)
 
-        img_url = redditData['data']['children'][0]['data']['url']
-
-        img_name = "{}.png".format(img_url[img_url.rfind("/")+1:img_url.rfind(".")])
+    @memes.command()
+    async def top_dank(self,ctx):
         try:
-            await filedownloader.save_file(img_url, img_name)
-        except asyncio.TimeoutError:
-            await ctx.send("Image is too big.")
-            os.remove(img_name)
-            return
-        except ValueError:
-            await ctx.send("Invalid link.")
-            return
-        await ctx.channel.send(file=discord.File(img_name))
-        os.remove(img_name)
+            link =  next(self.dankTopLinkGenerator)
+            await self.send_picture(ctx, link)
+        except StopIteration:
+            self.dankTopLinkGenerator = await self.dankTopLinkGenerator('dankmemes')
+            await self.top_dank(ctx)
 
 
+    async def send_picture(self, ctx, link):
+        # make sure: reddit image, not gif.
+        if 'i.redd.it' in link:
+            file_format = link.rsplit(".", 1)[1]
+            if 'jpg' in file_format or 'png' in file_format or 'jpeg' in file_format:
+                file_name = link.rsplit("/", 1)[1]
+                await filedownloader.save_file(link, "binary", file_name)
+                await ctx.send(file=discord.File(file_name))
+                os.remove(file_name)
+        else:
+            self.dank(ctx) # Try another link
 
+
+    def memeLinkHotGenerator(self, subreddit:str):
+        for submission in self.reddit.subreddit(subreddit).hot():
+            if not submission.stickied:
+                yield submission.url
+
+    def memeLinkTopGenerator(self, subreddit:str):
+        for submission in self.reddit.subreddit(subreddit).top('day'):
+            if not submission.stickied:
+                yield submission.url
+
+    @tasks.loop(hours=1.0)
+    async def resetHotGenerator(self):
+        self.dankHotLinkGenerator = await self.memeLinkHotGenerator('dankmemes')
+
+    @tasks.loop(hours=24.0)
+    async def resetTopGenerator(self):
+        self.dankTopLinkGenerator = await self.memeLinkTopGenerator('dankmemes')
 
 
 def setup(bot):
     bot.add_cog(misc(bot))
-    bot.add_cog(memes(bot))
+    bot.add_cog(reddit(bot))
 
 def teardown(bot):
     bot.remove_cog(misc(bot))
-    bot.remove_cog(memes(bot))
+    bot.remove_cog(reddit(bot))
